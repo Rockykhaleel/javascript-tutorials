596af89d1bfc607c8f8224f4ccc9d56bd05955e0
Rockykhaleel <r3khaleelshariff@gmail.com>
2024-01-15 14:34:12 +0530

    javascript tutorial

---
+++ js.js
@@ -0,0 +1,10 @@
+let phoneNumbers = ''
+
+// Split the string into an array of numbers
+let numbersArray = phoneNumbers.split(',');
+
+// Join the array into a string with line breaks
+let formattedNumbers = numbersArray.join('\n');
+
+console.log(formattedNumbers);
+
---
+++ practice.js
@@ -0,0 +1,115 @@
+//variables
+var a = 1; // var
+let b = 2; // let
+const c = 3; //const
+
+// console.log(c);
+// setTimeout( logged,2000); //async method
+// console.log(c+=b);
+// console.log(b);
+
+
+//data types
+
+let d = 123;//numbers
+let e = 12345678900000000000;//bigInt
+let f = 'string'//single quotes
+let g = "string"//double quotes
+let h = `string ${f} ${g}`//back tics
+let i = false;
+if(i==true){
+    console.log("its true");
+}
+let j = undefined;
+let k = null;
+
+let l = [1,2,3,4];
+
+// let m = prompt("Enter you name");
+
+// let n = confirm(`Are you sure, you are ${m} person?`);
+
+// console.log(n);
+// if(n==true){
+//     alert(`Hello ${m}`)
+// }else{
+//     alert("Please confirm your indentity")
+// }
+
+// const z = f +" " + g;
+// console.log(h);
+
+//functions
+// function logged(){
+//     console.log(a);
+// }
+
+
+//type convertions
+
+let z = '1';
+let y = '1';
+// console.log(Number(z)+Number(y));
+// console.log(typeof(z));
+// let x = parseInt(z)
+// console.log(typeof(x));
+
+
+// let v = 123456;
+// console.log(typeof(v));
+// let w = v.toString()
+// console.log(typeof(w));
+
+ 
+//Arithmetic Operators
+
+//+ add
+//- sub
+//* mul
+// / div
+// % mod
+// ** exponential
+// ++ increment
+// -- decrement
+
+// Assignment Operators
+// = 
+
+//Comparison Operators 
+
+// == 
+// if(a === '1'){
+//     console.log("Hello");
+// }
+// ===
+//>
+//<
+//>=
+//<=
+
+// Logical Operators
+
+// || or
+// && and
+/* ! not */
+// if(a == b || a == c){
+//     console.log(a);
+// }
+
+// if(a == b && a == c){
+//     console.log(a);
+// }
+
+// if(a != 2){
+//     console.log("yes");
+// }
+
+// if(condition){
+//     -----
+// }else if(condition){
+//     ----
+// }else{
+//     ---
+// }
+
+// a<b ? console.log("a is big") : console.log("b is big"); // ternary operator
